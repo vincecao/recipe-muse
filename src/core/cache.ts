639a7598c0975c116f769b87f4b7f8aab9316fd9
Lineng Cao <lineng.ca@gmail.com>
2025-02-18 22:19:21 -0800

    chore: Adjust cache config

--- src/core/cache.ts
+++ src/core/cache.ts
@@ -4,4 +4,4 @@ import { firebaseDb } from "~/app/api/_services/firebase";
 export const cachedRecipes = cache(() => firebaseDb.getAllRecipes());
 export const cachedRecipeById = cache((id: string) => firebaseDb.getRecipe(id));
 
-export const CACHE_EXPIRATION = 600 // in seconds
+export const CACHE_EXPIRATION = 3600000 // in seconds
